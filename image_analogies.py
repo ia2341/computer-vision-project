d536be5c2368931518b59261e912f7e151116838
Ishaan Agrawal <ia2341@columbia.edu>
2018-12-11 11:11:26 -0500

    Added color transfer methods to source code

---
+++ image_analogies.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+import scipy as sp
+from PIL import Image
+
+
+def color_transfer(source, target):
+    # convert color space from BGR to L*a*b color space
+    # note - OpenCV expects a 32bit float rather than 64bit
+    source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB).astype("float32")
+    target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB).astype("float32")
+    # compute color stats for both images
+    (smean, scov) = image_stats(source)
+    (tmean, tcov) = image_stats(target)
+    
+    # cholesky
+    w_s,v_s = np.linalg.eig(scov)
+    eig_s = sp.linalg.sqrtm(np.diag(w_s))
+    w_t,v_t = np.linalg.eig(tcov)
+    eig_t = sp.linalg.sqrtm(np.diag(w_t))
+    
+    s_root = np.matmul(v_s, eig_s)
+    s_root = np.matmul(s_root, v_s.transpose())
+    t_root = np.matmul(v_t, eig_t)
+    t_root = np.matmul(t_root, v_t.transpose())
+    t_inv = np.linalg.inv(t_root)
+    
+    A = np.matmul(s_root, t_inv)
+    b_act = smean - (np.matmul(A, tmean))
+    
+    (r, g, b) = cv2.split(target)
+    new_img = target.copy()
+    
+    m,n = r.shape
+    for x in range(m):
+      for y in range(n):
+        x_val = np.matrix([[r[x][y], g[x][y], b[x][y]]]).transpose()
+        x_new = np.matmul(A, x_val) + b_act
+        new_img[x][y][0] = x_new[0][0]
+        new_img[x][y][1] = x_new[1][0]
+        new_img[x][y][2] = x_new[2][0]
+    
+    
+    # converting back to BGR
+    transfer = cv2.cvtColor(new_img.astype("uint8"), cv2.COLOR_RGB2BGR)
+    return transfer
+
+
+def image_stats(image):
+    # compute mean and standard deviation of each channel
+    (r, g, b) = cv2.split(image)
+
+    rMean = np.average(r)
+    gMean = np.average(g)
+    bMean = np.average(b)
+    mean = np.matrix([[rMean, gMean, bMean]]).transpose()
+    cov = np.matrix([[0.0, 0.0, 0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]])
+    m,n = r.shape
+    for x in range(m):
+      for y in range(n):
+        x_val = np.matrix([[r[x][y], g[x][y], b[x][y]]]).transpose()
+        temp_one = (x_val - mean)
+        temp_two = np.matrix(temp_one).transpose()
+        sum_val = ((np.matmul(temp_one, temp_two)) / (m*n))
+        cov += sum_val
+
+    return (mean, cov)
+
+
+def main():
+    target = cv2.imread(style_path)
+    source = cv2.imread(content_path)
+
+    # transfer of color
+    transfer = color_transfer(source, target)
+
+    im = Image.fromarray(transfer)
+    new_style_path = 'output/transfer.jpg'
+    im.save(new_style_path)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ luminance.py
@@ -0,0 +1,72 @@
+import numpy as np
+import cv2
+from PIL import Image
+
+
+def color_transfer(source, target):
+    # convert color space from BGR to L*a*b color space
+    # note - OpenCV expects a 32bit float rather than 64bit
+    source = cv2.cvtColor(source, cv2.COLOR_BGR2LAB).astype("float32")
+    target = cv2.cvtColor(target, cv2.COLOR_BGR2LAB).astype("float32")
+
+    # compute color stats for both images
+    (lMeanSrc, lStdSrc, aMeanSrc, aStdSrc, bMeanSrc, bStdSrc) = image_stats(source)
+    (lMeanTar, lStdTar, aMeanTar, aStdTar, bMeanTar, bStdTar) = image_stats(target)
+    
+    # split the color space
+    (l, a, b) = cv2.split(target)
+    
+    # substarct the means from target image
+    l -= lMeanTar
+    a -= aMeanTar
+    b -= bMeanTar
+    
+    # scale by the standard deviation
+    l = (lStdTar/lStdSrc)*l
+    a = (aStdTar/aStdSrc)*a
+    b = (bStdTar/bStdSrc)*b
+    
+    # add the source mean
+    l += lMeanSrc
+    a += aMeanSrc
+    b += bMeanSrc
+    
+    # clipping the pixels between 0 and 255
+    l = np.clip(l, 0, 255)
+    a = np.clip(a, 0, 255)
+    b = np.clip(b, 0, 255)
+    
+    # merge the channels
+    transfer = cv2.merge([l, a, b])
+    
+    # converting back to BGR
+    transfer = cv2.cvtColor(transfer.astype("uint8"), cv2.COLOR_LAB2BGR)
+    return transfer
+
+
+def image_stats(image):
+    # compute mean and standard deviation of each channel
+    (l, a, b) = cv2.split(image)
+
+    lMean, lStd = (np.average(l), np.std(l))
+    aMean, aStd = (np.average(a), np.std(a))
+    bMean, bStd = (np.average(b), np.std(b))
+
+    return (lMean, lStd, aMean, aStd, bMean, bStd)
+
+
+def main():
+    target = cv2.imread(style_path)
+    source = cv2.imread(content_path)
+
+    # transfer of color
+    transfer = color_transfer(source, target)
+
+    im = Image.fromarray(transfer)
+    new_style_path = 'output/transfer.jpg'
+    im.save(new_style_path)
+
+
+
+if __name__ == '__main__':
+    main()
